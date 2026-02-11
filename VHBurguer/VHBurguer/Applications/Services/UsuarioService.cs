aa3af46b1a9d4c5499f199a162b97fdbb7477ff7
EDUARDO BERNAL DA SILVA <44438098807@info.local>
2026-02-11 10:43:23 -0300

    DomainException

--- VHBurguer/VHBurguer/Applications/Services/UsuarioService.cs
+++ VHBurguer/VHBurguer/Applications/Services/UsuarioService.cs
@@ -1,13 +1,16 @@
 ﻿using VHBurguer.DTOs;
 using VHBurguer.Interfaces;
 using VHBurguer.Domains;
+using VHBurguer.Exceptions;
+using System.Security.Cryptography;
+using System.Text;
 namespace VHBurguer.Applications.Services
 {
     // Service Concentra o "como fazer"
     public class UsuarioService
     {
-     // repository e o canal para acessar os dados 
-     private readonly IUsuarioRepository _repository;
+        // repository e o canal para acessar os dados 
+        private readonly IUsuarioRepository _repository;
         // injeção de dependência
         //implementamos o repositorio e o service sp  depende da interface do repositorio
 
@@ -15,7 +18,7 @@ namespace VHBurguer.Applications.Services
         {
             _repository = repository;
         }
-            
+
         private static LerUsuarioDto LerDto(Usuario usuario)
         {
             LerUsuarioDto usuarioDto = new LerUsuarioDto
@@ -33,6 +36,38 @@ namespace VHBurguer.Applications.Services
             List<Usuario> usuarios = _repository.Listar();
             List<LerUsuarioDto> usuariosDto = usuarios.Select(usuarioBanco => LerDto(usuarioBanco)).ToList();
             return usuariosDto;
+        }
+
+        private static void ValidarEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+            {
+                throw new DomainException("Email inválido.");
+            }
+
+        }
+
+        private static byte[] HashSenha(string senha)
+        {
+            if (string.IsNullOrWhiteSpace(senha))
+            {
+                throw new DomainException("Senha é obrigatoria.");
+            }
+
+            using var sha256 = SHA256.Create(); // Gera um hash da senha usando SHA256 e devolve em byte
+            return sha256.ComputeHash(Encoding.UTF8.GetBytes(senha));
+        }
+
+        public LerUsuarioDto ObterPorId(int id)
+        {
+            Usuario usuario = _repository.ObterPorId(id);
+
+            if (usuario == null)
+            {
+                throw new DomainException("Usuario não encontrado.");
+            }
+
+            return LerDto(usuario);
         }
     }
 }
